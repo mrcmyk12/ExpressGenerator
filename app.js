9bba4c5a48ee8c528a3449f48dfde51158a1cb89
Philip Sanders <mrcmyk12@gmail.com>
2021-07-06 03:51:06 -0700

    File Upload

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ var usersRouter = require("./routes/users");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
+const uploadRouter = require("./routes/uploadRouter");
 const { read } = require("fs");
 
 var app = express();
@@ -64,6 +65,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
